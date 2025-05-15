f5b477289742020a80d325e0b07da61c519adf09
Jack <jack4shihong@gmail.com>
2025-05-15 13:33:08 +0800

    add clip

---
+++ algorithms/WeCLIP/clip/__init__.py
@@ -0,0 +1,18 @@
+"""
+__init__.py 对CLIP算法进行初始化
+
+    @Time    : 2025/05/13
+    @Author  : JackWang
+    @File    : __init__.py
+    @IDE     : VsCode
+    @Copyright Copyright Shihong Wang (c) 2025 with GNU Public License V3.0
+"""
+
+# Standard Library
+
+# Third-Party Library
+
+# Torch Library
+
+# My Library
+from .clip import *
